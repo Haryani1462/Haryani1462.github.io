77011f2948278c7fab7a2eaf783637b595ccc87a
Haryani1462 <43215984+Haryani1462@users.noreply.github.com>
2018-11-28 22:12:46 -0600

    Add files via upload

--- mad.js
+++ mad.js
@@ -1,7 +1,6 @@
 function array1()
 {
     var inputs = [];
-    inputs.push(document.getElementById('adj1').value);
     inputs.push(document.getElementById('adj2').value);
     inputs.push(document.getElementById('adj3').value);
     inputs.push(document.getElementById('adj4').value);
@@ -11,13 +10,15 @@ function array1()
     inputs.push(document.getElementById('adj8').value);
     inputs.push(document.getElementById('adj9').value);
     inputs.push(document.getElementById('adj10').value);
+    inputs.push(document.getElementById('adj11').value);
+    inputs.push(document.getElementById('adj12').value);
+    inputs.push(document.getElementById('adj13').value);
+    inputs.push(document.getElementById('adj14').value);
 
-    return "Hello kids! I am " + inputs[0] + "and I will teach you how to bake a " + inputs[1] + "pie." +
-        "First, you need to bake the pie at " + input[2] + "degrees." +
-        "Next, make sure you go to your nearest " + inputs[3] + " and pick up some " + input[4] + " to put on your pie!";
+    return "Spring vacation usually falls around Easter time. The schools are closed and all the " + inputs[9] + " get " + inputs[0] + " weeks off. The " + inputs[2] + " teachers also get a vacation. There are lots of things to do during Easter vacation. Some kids loaf around and watch the " + inputs[7] + " . Others get outside and play " + inputs[1] + " , while more ambitious students spend their time studying their " + inputs[3] + " books so they will grow up to become " + inputs[10] + " . Little kids also color " + inputs[5] + " eggs. Here's how you color an egg: first, mix a package of " + inputs[4] + " dye in a bowl full of " + inputs[11] + " . Then, dip a " + inputs[8] + " in the bowl and rinse it off with " + inputs[12] + ". Then, after it dries, you can paint it with a brush. Then you show it to your friends, who will say, Boy what a " + inputs[6] + " egg!"
 
 
-
+/* story from redkids madlibs */
 
 
 
