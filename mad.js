6f1c8f21b5a4fea5afdcc40efab67923212069ec
Haryani1462 <43215984+Haryani1462@users.noreply.github.com>
2018-11-28 21:47:13 -0600

    Add files via upload

---
+++ mad.js
@@ -0,0 +1,25 @@
+function array1()
+{
+    var inputs = [];
+    inputs.push(document.getElementById('adj1').value);
+    inputs.push(document.getElementById('adj2').value);
+    inputs.push(document.getElementById('adj3').value);
+    inputs.push(document.getElementById('adj4').value);
+    inputs.push(document.getElementById('adj5').value);
+    inputs.push(document.getElementById('adj6').value);
+    inputs.push(document.getElementById('adj7').value);
+    inputs.push(document.getElementById('adj8').value);
+    inputs.push(document.getElementById('adj9').value);
+    inputs.push(document.getElementById('adj10').value);
+
+    return "Hello kids! I am " + inputs[0] + "and I will teach you how to bake a " + inputs[1] + "pie." +
+        "First, you need to bake the pie at " + input[2] + "degrees." +
+        "Next, make sure you go to your nearest " + inputs[3] + " and pick up some " + input[4] + " to put on your pie!";
+
+
+
+
+
+
+
+}
